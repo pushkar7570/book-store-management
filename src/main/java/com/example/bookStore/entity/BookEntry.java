41185605efc70a10052bea52c1d41c69f6c3255a
pushkar7570 <singh.pushkar7570@gmail.com>
2025-01-10 01:32:38 +0530

    init commit

---
+++ src/main/java/com/example/bookStore/entity/BookEntry.java
@@ -0,0 +1,77 @@
+package com.example.bookStore.entity;
+
+public class BookEntry {
+
+    private Long id;
+    private String title;
+    private String author;
+    private Double price;
+    private Double rating;
+    private String description;
+    private Integer quantity;
+    private Long categoryId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
